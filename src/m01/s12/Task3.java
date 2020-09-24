34c7c2f95f7818f76736794213b6a8c23e180ee4
Bledhgarm <kurutta.grind@gmai.com>
2020-09-24 19:01:24 +0500

    Solved task

--- src/m01/s12/Task3.java
+++ src/m01/s12/Task3.java
@@ -3,9 +3,15 @@ package m01.s12;
 import java.util.Scanner;
 
 public class Task3 {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int number = in.nextInt();
-        System.out.println(number + 2 - number % 2);
+        if (number >= 0 && number < 10) {
+            System.out.println("DIGIT");
+        }
+        else if (number > 9 && number <= 99) {
+            System.out.println("NUM");
+        }
+        else System.out.println("OTHER");
     }
 }
